40a104d846199045cb4df3e710c6936397bf0415
NJKITTENS555 <nicholaswiney@gmail.com>
2025-09-24 16:18:17 +0000

    creaes data types

---
+++ lessson01.py
@@ -0,0 +1,30 @@
+# this is a comment
+# data types
+
+print ("hello world")
+print (" once apon a time \nand a very good time it was \nthere was a moocow ")
+
+# INTEGERS
+
+print(42)
+print(-23)
+print(5+5-11)
+
+#FLOAT
+
+print(4.0)
+print(2.0 + 4.5)
+print(2.0 + 4) # RETUNS A FLOAT
+
+# BOOLEAN
+
+print(True) #1
+print(False) #0
+print (1 ==2 )
+print(True - True)
+print(1 != 2)
+print( "Hello" == "hello")
+print(5+ 5 == 11 )
+print(type( "To see this message, wrape me in a print statement"))
+print(type(3))
+print(" Name: \tBilly \nAge: \t14 \nGrade: \t9")
