4124875b6c7f7bbc516eb7b82121b4c2672ab69b
NJKITTENS555 <nicholaswiney@gmail.com>
2025-09-25 17:26:30 +0000

    coom m

---
+++ variables_lesson02.py
@@ -0,0 +1,65 @@
+
+
+taco = "hello world"
+
+password = "drowssap321"
+email = "winey20293620@student.nhvweb.net"
+
+print("password:" ,password)
+
+tempiture = 72.5
+print(type(tempiture))
+price = 3.99
+
+enabled = False
+Is_complete = True
+print(Is_complete)
+enabled = True
+print(enabled)
+
+
+
+
+x = 3.14
+y = 7
+print(x + y)
+
+count = 10
+print(count)
+countdown = count - 1
+print(countdown)
+count = countdown
+print(count)
+countdown = count - 1
+print(countdown)
+count = countdown
+print(count)
+
+
+# change x, y, and z to more descriptive names
+
+x = "Radia_Perlmen"
+y = 34 
+z = "Networking Engineer"
+
+RaP = x
+y  = y
+NeEn = z
+
+count = 10
+count_2 = count + 5
+print(count_2)
+
+#3
+
+x = 4
+y = "hello"
+xx = y
+y = x
+x = xx
+
+#Challenge 4
+
+Is_raining = False
+Is_raining = True
+print( "Is it raining?: ",Is_raining)
