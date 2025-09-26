6bcff97c7f7e661d5c3b8114f48ac2bb04b74794
NJKITTENS555 <nicholaswiney@gmail.com>
2025-09-26 18:02:27 +0000

    jef

---
+++ lesson03math.py
@@ -0,0 +1,38 @@
+# math operators: +, -, *, /, //, %, **, 
+add = 7 + 2 
+print("Sum: ", add)
+
+
+subtract = 7 - 2
+print("Difference: ", subtract)
+
+multiply = 7 * 2
+print("product: ", multiply)
+
+float_divide = 7 / 2
+print(" float division: ", float_divide) 
+
+intiger_divide = 7 // 2
+print("Intifer Division: ", intiger_divide)
+
+modulus = 7 % 2
+print("modulus: ", modulus)
+
+exponet = 7 ** 2
+print("Exponet:", exponet)
+
+print(2 + 2 != 10 - 6)
+result = (2 + 3) * 4
+print( "Result 1:", result)
+
+result2 = 2 ** 3 * 4
+print("result 2:", result2)
+
+result3 = 20 / 5 * 2
+print("Result 3 :", result3)
+
+result4 = 10 - 2 + 3
+print("Result 4:", result4)
+
+result5 = 5 + 2 ** 3 * (4 - 1)
+print("Result 5: ", result5)
