345f87ead89b5d2ce61da334d08d59339b39e613
NJKITTENS555 <nicholaswiney@gmail.com>
2025-10-14 16:27:54 +0000

    commikt

--- lesson06_strings.py
+++ lesson06_strings.py
@@ -28,4 +28,37 @@ print(length_of_word)
 phrase = "SpoNgEbOb"
 print("\nUppercase:", phrase)
 print("\nlowercase:", phrase)
+print("Capitalized first letter:", phrase.capitalize())
+print("Title Format:", phrase.title())
+
+
+#Find and replace text
+sentence = "I'm a goofy goober"
+new_sentence = sentence.replace("I'm", "You're")
+new_replacment = sentence.replace("goofy", "goober")
+print(sentence)
+print(new_sentence)
+print(new_replacment)
+
+name = "Ada"
+age = 28
+city = "london"
+
+print(f"Hello, my name is {name}. I am {age} yeras old and live in {city}.")
+# f strings can do math inside {}
+
+print(f"next year I will be {age + 1}. My name in uppercase is {name.upper()}")
+
+# Challenge 1: Favorite Quote
+# Ask the user for their favorite quote and display its length.
+# EXAMPLE OUTPUT:
+# Enter your favorite quote: Those who believe in telekinetics, raise my hand.
+# Your quote is 48 characters long.
+
+Favorite_quote = input("What is your favorite quote?: ")
+print( f"quote is {len(Favorite_quote)} charicters long")
+
+firstname = input("what is your first name:")
+lastname = input("what is your last name:")
+print(f"{lastname}, {firstname}")
 
