9f4a023d5716e16c6b64775aeae6df746d81cffc
NJKITTENS555 <nicholaswiney@gmail.com>
2025-10-27 17:03:07 +0000

    nedyaj

--- lesson-09-loops.py
+++ lesson-09-loops.py
@@ -9,22 +9,78 @@ print("--- Loops in Python ---")
 # The for-loop.
 # A for-loop repeats for each element in a sequence (like a list or string).
 import time 
-animals = ["lamb", "sheep", "cow", "goose", "donkey" ]
+#animals = ["lamb", "sheep", "cow", "goose", "donkey" ]
 
-print("\nOur animals:", animals)
-print("\n--- For Loop: visiting each animal ---")
+#print("\nOur animals:", animals)
+#print("\n--- For Loop: visiting each animal ---")
 
-for animal in animals:
-    print("Now petting a", animal)
-    time.sleep(1.5)
+#for animal in animals:
+#    print("Now petting a", animal)
+#    time.sleep(1.5)
 
-    if animal =="sheep":
-        print("hi shep!!")
-print("Now I have pet all the animals!")
+ #   if animal =="sheep":
+  #      print("hi shep!!")
+# print("Now I have pet all the animals!")
 
-for i in range(5):
-    print("counting:", i)
+#for i in range(5):
+ #   print("counting:", i)
 
 # range(start, stop, step)
-print()
+#print()
 print("----range() with start, stop, step ----")
+
+print("--- Iterating over strings ---\n")
+
+fav_word = "Shenanigan"
+
+for letter in fav_word:
+    print(letter)
+print("--- Iterating over strings ---\n")
+
+fav_word = "Shenanigan"
+
+for letter in fav_word:
+    print(letter, end = " ")
+    fav_word = "Shenanigan"
+letter_list = []
+
+for letter in fav_word:
+    print(letter, end="")
+   # letter.append(letter_list)
+    print(letter_list)
+
+print()
+
+# ---------------------------------------------------------
+# WHILE LOOPS
+# ---------------------------------------------------------
+# A while-loop repeats *while* a condition is true.
+# If you forget to change the condition, it loops forever.
+# And then your program becomes immortal. Avoid that.
+
+# += to add to a variable, -= to subtract to a variable, = to overright 
+import time
+count = 0
+
+#while count < 5:
+ #   print(f"Loopin'. We are on loop # {count}.")
+  #  count += 1
+   # time.sleep(0.5)
+
+#print("We have escaped loop!!")
+#user_input = ""
+
+#while user_input == "exit":
+#    user_input =  input("type 'exit' to escape :") 
+ #   user_input = ""
+
+count = 60
+increment = 1
+
+while count > 0:
+    if count < 0: 
+        break
+    count -= increment
+    increment += 1
+    print(count)
+
