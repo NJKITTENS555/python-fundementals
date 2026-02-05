bf74d711819784b17c2a76a01ee70cae0c1524af
NJKITTENS555 <nicholaswiney@gmail.com>
2026-02-05 18:33:33 +0000

    ewrqq

--- lesson 10 challenges.py
+++ lesson 10 challenges.py
@@ -49,3 +49,32 @@ def is_even(e):
 
 joe = is_even(627831)
 print(joe)
+    
+    
+def analyze_word(word): 
+     char = word.lower()
+     vowelCount = 0
+     consonantCount = 0
+
+
+    #BASE CASE
+     if type(word) != str: 
+         print("Error")
+         return 0
+     
+     
+     for i in word.lower():
+         print(i)
+        #  char = word[i]
+        #  print(char)
+         if i in "aeiou":
+             vowelCount += 1
+         elif i.isalpha():
+             consonantCount += 1
+    
+     print(vowelCount)
+     print(consonantCount)
+
+# DEBUG
+
+analyze_word("")
